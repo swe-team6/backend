171f9e566113dfd418a537c1ca509be33399fd50
Olzhassss <52438129+Olzhassss@users.noreply.github.com>
2023-11-16 23:18:40 +0600

    ApplicationRun: add extends Servlet

--- src/main/java/com/vms/demo/ApplicationRun.java
+++ src/main/java/com/vms/demo/ApplicationRun.java
@@ -2,9 +2,10 @@ package com.vms.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplicationRun {
+public class ApplicationRun extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationRun.class, args);
