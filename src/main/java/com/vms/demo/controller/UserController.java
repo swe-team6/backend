89d7edf5f76426b0f4183750ce7cacb1e288cbb2
Olzhassss <52438129+Olzhassss@users.noreply.github.com>
2023-11-18 16:48:59 +0600

    Controllers: UserController: fix

--- src/main/java/com/vms/demo/controller/UserController.java
+++ src/main/java/com/vms/demo/controller/UserController.java
@@ -1,14 +1,15 @@
 package com.vms.demo.controller;
 
-import com.vms.demo.dto.User;
-import com.vms.demo.repository.UserRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.vms.demo.entity.UserEntity;
+import com.vms.demo.repository.UserRepository;
 
 @RestController
 public class UserController {
@@ -16,14 +17,14 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/getAllUsers")
-    public ResponseEntity<List<User>> getAllUsers(){
-        try{
-            List<User> users = new ArrayList<>(userRepository.findAll());
-            if(users.isEmpty()){
+    public ResponseEntity<List<UserEntity>> getAllUsers() {
+        try {
+            List<UserEntity> users = userRepository.findAll();
+            if (users.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
             return new ResponseEntity<>(users, HttpStatus.OK);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
