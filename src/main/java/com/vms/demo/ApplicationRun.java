4183b44f17e1ff822db42c7ba45a2c204147fc8a
Olzhassss <52438129+Olzhassss@users.noreply.github.com>
2023-11-18 14:22:59 +0600

    ApplicationRun: logger

--- src/main/java/com/vms/demo/ApplicationRun.java
+++ src/main/java/com/vms/demo/ApplicationRun.java
@@ -1,5 +1,7 @@
 package com.vms.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,6 +9,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class ApplicationRun extends SpringBootServletInitializer {
+
+	private static final Logger log = LoggerFactory.getLogger(ApplicationRun.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationRun.class, args);
