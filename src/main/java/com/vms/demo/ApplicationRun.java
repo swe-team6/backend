f3028a01fdcf96567fc77476a5174ec90c52c0b8
saide <saida.tulebayeva@nu.edu.kz>
2023-11-19 19:38:50 +0600

    ApplicationController add, EnableWebSecurity add. Check application.properties for name and password.

--- src/main/java/com/vms/demo/ApplicationRun.java
+++ src/main/java/com/vms/demo/ApplicationRun.java
@@ -6,8 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class ApplicationRun extends SpringBootServletInitializer {
 
 	private static final Logger log = LoggerFactory.getLogger(ApplicationRun.class);
---
+++ src/main/java/com/vms/demo/controller/ApplicationController.java
@@ -0,0 +1,15 @@
+package com.vms.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rest/auth")
+public class ApplicationController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "HEYYYYYY";
+    }
+}
