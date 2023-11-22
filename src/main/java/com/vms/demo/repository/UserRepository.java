28211ba644a841bf3c583b5d3fd4231d0fa08c5b
Olzhassss <52438129+Olzhassss@users.noreply.github.com>
2023-11-22 21:18:51 +0600

    Repo: User: fix types

--- src/main/java/com/vms/demo/repository/UserRepository.java
+++ src/main/java/com/vms/demo/repository/UserRepository.java
@@ -6,13 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.vms.demo.dto.User;
 import com.vms.demo.entity.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    List<User> findByUserID(Long userID);
+    List<UserEntity> findByUserID(Long userID);
 
     @Transactional
     void deleteByUserID(Long userID);
