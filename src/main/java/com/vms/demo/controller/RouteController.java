7f174323842941c95007b31366f48d3699667a44
Olzhassss <52438129+Olzhassss@users.noreply.github.com>
2023-11-22 20:42:13 +0600

    Controller: Route: PUT update endpoint

--- src/main/java/com/vms/demo/controller/RouteController.java
+++ src/main/java/com/vms/demo/controller/RouteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.vms.demo.dto.route.RouteCreateDTO;
 import com.vms.demo.dto.route.RouteDTO;
+import com.vms.demo.dto.route.RouteUpdateDTO;
 import com.vms.demo.service.RouteService;
 
 import jakarta.validation.Valid;
@@ -38,5 +40,11 @@ public class RouteController {
     @ResponseStatus(HttpStatus.CREATED)
     public RouteCreateDTO create(@Valid @RequestBody RouteCreateDTO routeCreateDTO) {
         return routeService.createRoute(routeCreateDTO);
+    }
+
+    @PutMapping("{routeID}")
+    @ResponseStatus(HttpStatus.OK)
+    public RouteDTO update(@PathVariable Long routeID, @RequestBody RouteUpdateDTO routeUpdateDTO) {
+        return routeService.updateRoute(routeID, routeUpdateDTO);
     }
 }
