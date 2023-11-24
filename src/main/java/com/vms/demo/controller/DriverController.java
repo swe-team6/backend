5d48351399e16be073adb32a19062037b68b690f
Olzhassss <52438129+Olzhassss@users.noreply.github.com>
2023-11-24 12:59:01 +0600

    Controller: driver: reverting to PUT

--- src/main/java/com/vms/demo/controller/DriverController.java
+++ src/main/java/com/vms/demo/controller/DriverController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -48,7 +49,7 @@ public class DriverController {
         return driverService.createDriver(driverCreateDTO);
     }
 
-    @PostMapping("{driverID}/update")
+    @PutMapping("{driverID}")
     @ResponseStatus(HttpStatus.OK)
     public DriverFullDTO update(@PathVariable Long driverID, @RequestBody DriverUpdateDTO driverUpdateDTO) {
         return driverService.updateDriver(driverID, driverUpdateDTO);
