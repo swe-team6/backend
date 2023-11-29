6505eb16b7f2b2e652850fd3e1bdf1cd40621454
Olzhassss <52438129+Olzhassss@users.noreply.github.com>
2023-11-29 14:15:34 +0600

    Entity: User: Fix chat

--- src/main/java/com/vms/demo/entity/UserEntity.java
+++ src/main/java/com/vms/demo/entity/UserEntity.java
@@ -4,7 +4,16 @@ import java.util.Set;
 
 import com.vms.demo.types.RoleType;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -47,7 +56,7 @@ public class UserEntity {
     private DriverEntity driver;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
-    private DriverEntity chat;
+    private ChatEntity chat;
 
     @OneToMany(mappedBy = "maintainer", cascade = CascadeType.ALL)
     private Set<MainJobEntity> maintenanceJobs;
