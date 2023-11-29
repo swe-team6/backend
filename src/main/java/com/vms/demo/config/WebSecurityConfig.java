455664823841c84247e74b5f15b2676ab0a75a66
Olzhassss <52438129+Olzhassss@users.noreply.github.com>
2023-11-29 14:14:36 +0600

    DTO: UserDTO

--- src/main/java/com/vms/demo/config/WebSecurityConfig.java
+++ src/main/java/com/vms/demo/config/WebSecurityConfig.java
@@ -1,15 +1,9 @@
 package com.vms.demo.config;
 
-import com.vms.demo.dto.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,20 +19,21 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 
-//	@Bean
-//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//		http.authorizeHttpRequests(
-//						requests ->
-//								requests
-//										.requestMatchers(HttpMethod.GET, "/users")
-//										.hasRole("USER")
-//										.requestMatchers(HttpMethod.GET, "/admins")
-//										.hasRole("ADMIN")
-//										.anyRequest()
-//										.authenticated())
-//				.formLogin(Customizer.withDefaults());
-//		return http.build();
-//	}
+	// @Bean
+	// public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+	// Exception {
+	// http.authorizeHttpRequests(
+	// requests ->
+	// requests
+	// .requestMatchers(HttpMethod.GET, "/users")
+	// .hasRole("USER")
+	// .requestMatchers(HttpMethod.GET, "/admins")
+	// .hasRole("ADMIN")
+	// .anyRequest()
+	// .authenticated())
+	// .formLogin(Customizer.withDefaults());
+	// return http.build();
+	// }
 	// @Bean
 	// public UserDetailsService userDetailsService() {
 	// UserDetails user = User.withDefaultPasswordEncoder()
--- src/main/java/com/vms/demo/dto/UserDTO.java
+++ src/main/java/com/vms/demo/dto/UserDTO.java
@@ -1,21 +1,29 @@
 package com.vms.demo.dto;
 
+import com.vms.demo.types.RoleType;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
-public class User {
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
     private Long userID;
-    //private Role role;
+    private RoleType role;
     private String phoneNumber;
     private String address;
     private String lastName;
     private String middleName;
     private String firstName;
-    private String password;
     private Long govID;
     private String email;
+    private Long chatID;
+    private String pictureUrl;
+
 }
